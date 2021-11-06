13aa9bcd08039a89df81706f52775313a49a6ced
Trinhtruc1831 <19521059@gm.uit.edu.vn>
2021-11-06 13:27:05 +0700

    Model part Truc

---
+++ IS220.M11/IS220.M11/Models/interestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS220.M11.Models
+{
+    public class interestModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InPostID { get; set; }
+        [ForeignKey("PostID")]
+        public postModel posts{ get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string InUserID { get; set; }
+        [ForeignKey("UserID")]
+        public accountModel accounts { get; set; }
+
+    }
+}
+
