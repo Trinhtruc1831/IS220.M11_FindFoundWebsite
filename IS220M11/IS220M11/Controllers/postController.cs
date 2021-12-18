587501a75fbd4bf8578637ddef92d6a1cb4c72ef
huuthang196 <19522200@gm.uit.edu.vn>
2021-12-18 22:03:54 +0700

    thang 18/12

--- IS220M11/IS220M11/Controllers/postController.cs
+++ IS220M11/IS220M11/Controllers/postController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace IS220M11.Controllers
@@ -31,6 +32,38 @@ namespace IS220M11.Controllers
             List<object> a = query.ToList<object>();
             return query;
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(List<IFormFile> files, [Bind("PostID,PTitle,PPrice")] postModel post, [Bind("ILink, IPostID")] pictureModel picture)
+        {
+            var size = files.Sum(f => f.Length);
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // var filePath = Path.Combine
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                post.PUserID = 1;
+                post.PDate = DateTime.Now;
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                picture.IPostID = post.PostID;
+                _context.Add(picture);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 
 }
