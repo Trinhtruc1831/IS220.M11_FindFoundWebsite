62e40faea64d1c82fd5dc4e3c255f3ad807f7c9d
Trinhtruc1831 <19521059@gm.uit.edu.vn>
2021-12-20 23:03:57 +0700

    Loginv1

--- IS220M11/IS220M11/Controllers/accountController.cs
+++ IS220M11/IS220M11/Controllers/accountController.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IS220M11.Controllers
 {
@@ -19,7 +23,7 @@ namespace IS220M11.Controllers
         {
             return _context.accounts;
         }
-        public string GetEmail()
+        public bool IsPass()
         {
             var query = from st in _context.accounts
                         select new
@@ -31,7 +35,72 @@ namespace IS220M11.Controllers
             {
                 EmailList.Add(ac.email);
             }
-            return EmailList[1];
+            /*return EmailList[1];*/
+            return true;
         }
+
+        /**************Account*****************/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (!string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("Login");
+            }
+            ClaimsIdentity identity = null;
+            bool isAuthenticate = false;
+            if (username == "admin" && password == "a")
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,"Admin")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticate = true;
+            }
+            if (username == "demo" && password == "c")
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,"User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticate = true;
+            }
+            if (isAuthenticate)
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        /*HOme*/
+        [Authorize(Roles = "Admin,User")]
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+        [Authorize(Roles = "User")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+
     }
+
 }
+
