200a0ea3765debe066aeaa2cbbf253c78982ccdc
huuthang196 <19522200@gm.uit.edu.vn>
2021-12-29 10:37:10 +0700

    addimage

--- IS220M11/IS220M11/Controllers/pictureController.cs
+++ IS220M11/IS220M11/Controllers/pictureController.cs
@@ -8,41 +8,71 @@ using Microsoft.EntityFrameworkCore;
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Web;
+using System.Diagnostics;
 
 namespace IS220M11.Controllers
 {
-    public class pictureController:Controller
+    public class pictureController : Controller
     {
         private readonly FindFoundContext _context;
-    
-        public pictureController(FindFoundContext context)
+
+        private readonly IWebHostEnvironment _webHostEnviroment;
+
+        public pictureController(FindFoundContext context,
+                IWebHostEnvironment webHostEnviroment)
         {
             _context = context;
+            _webHostEnviroment = webHostEnviroment;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.pictures.ToListAsync());
         }
-
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
         public IActionResult AddImage()
         {
             ViewData["username"] = HttpContext.Session.GetString("username");
+            ViewData["postid"] = HttpContext.Session.GetString("postid");
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddImage([Bind("ILink")] pictureModel picture, [Bind("PostID")] postModel post)
+        public async Task<IActionResult> AddImage(pictureModel pic)
         {
+            int postid = Int32.Parse(HttpContext.Session.GetString("postid"));
             if (ModelState.IsValid)
             {
-                picture.IPostID = post.PostID;
-                _context.Add(picture);
+                string folder = "";
+                if (pic.Image != null)
+                {
+                    folder = "public/assets/img/";
+                    folder += Guid.NewGuid().ToString() + "_" + pic.Image.FileName;
+                    string serverFolder = Path.Combine(_webHostEnviroment.WebRootPath, folder);
+
+                    await pic.Image.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+                }
+                int order = Int32.Parse(HttpContext.Session.GetString("Iorder"));
+                pic.IOrder = order;
+                order = order + 1;
+                HttpContext.Session.SetString("Iorder", order.ToString());
+
+                pic.ILink = "/" + folder;
+                pic.IPostID = postid;
+                await _context.pictures.AddAsync(pic);
                 await _context.SaveChangesAsync();
-                // return RedirectToAction(nameof(Index));
+                return RedirectToAction("AddImage", "picture");
             }
             ViewData["username"] = HttpContext.Session.GetString("username");
-            return View();
+            return Error();
+
         }
     }
 }
